39124c98945dec8f19b91bca59156954fc02b3df
Najla Gianneta Agisny <najla.gianneta.tif425@polban.ac.id>
2026-02-12 21:45:44 +0700

    Fundamental Programming Structures in Java

---
+++ DataTypes.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class DataTypes {
+    public static void main(String[] args) {
+       Scanner sc = new Scanner(System.in);
+       System.out.print("Enter the number of test cases: ");
+       int T = sc.nextInt();
+
+       for (int i = 0; i < T; i++) {
+        System.out.print("Enter the number: ");
+
+        try{
+            long n = sc.nextLong();
+            System.out.println(n + " can be fitted in:");
+
+            if (n >= Byte.MIN_VALUE && n<= Byte.MAX_VALUE)
+                System.out.println("* byte");
+
+            if (n >= Short.MIN_VALUE && n<= Short.MAX_VALUE)
+                System.out.println("* short");
+
+            if (n >= Integer.MIN_VALUE && n<= Integer.MAX_VALUE)
+                System.out.println("* int");
+
+            if (n >= Long.MIN_VALUE && n<= Long.MAX_VALUE)
+                System.out.println("* long");
+        } catch (Exception e) {
+            System.out.println(sc.next() + " can't be fitted anywhere.");
+        }
+       }
+       sc.close();
+    }
+}
+
