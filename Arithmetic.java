b4c6efda6b0734f00071d5d4c3c8f40ac4b699d9
njlgisny <najla.gianneta.tif425@polban.ac.id>
2026-02-12 21:52:53 +0700

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,8 @@
+public class Arithmetic {
+     public static void main(String[] args) {
+        double x = 92.98;
+        int nx = (int) Math.round(x);
+
+        System.out.println("Nilai nx setelah pembulatan: " + nx);
+    }
+}
